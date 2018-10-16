3026b1a30ec5b68776e1c672f01898a8b5a938ec
vector <vector4wang@qq.com>
2018-10-16 09:35:58 +0800

    local

---
+++ src/main/java/lambdasinaction/chap11/standard/Resume.java
@@ -0,0 +1,44 @@
+package lambdasinaction.chap11.standard;
+
+public class Resume {
+	private String company;
+
+	private String school;
+
+	private String skill;
+
+	private String title;
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+
+	public String getSkill() {
+		return skill;
+	}
+
+	public void setSkill(String skill) {
+		this.skill = skill;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+}
